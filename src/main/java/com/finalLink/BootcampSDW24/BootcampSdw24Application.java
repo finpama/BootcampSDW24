7637531afa2d13e10e12cf67f80dd0fbeb611f6c
finpama <phma020@gmail.com>
2024-03-27 05:54:27 -0300

    Init do Projeto

---
+++ src/main/java/com/finalLink/BootcampSDW24/BootcampSdw24Application.java
@@ -0,0 +1,13 @@
+package com.finalLink.BootcampSDW24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSdw24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSdw24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalLink/BootcampSDW24/BootcampSdw24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalLink.BootcampSDW24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSdw24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
